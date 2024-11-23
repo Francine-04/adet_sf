3ab0128f805b416052f7dccafad7ba251a3cc8fc
Francine-04 <fdelatorre@gbox.ncf.edu.ph>
2024-11-24 01:28:59 +0800

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, // Use the PORT environment variable or fallback to 3000
-  },
-});
-
+    host: '0.0.0.0',  
+    port: 10000,      
+    strictPort: true, 
+  }
+})
